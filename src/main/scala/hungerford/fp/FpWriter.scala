cd57db8af5347105ac7664cc3121079d4f60dca7
John Hungerford <john.hungerford@labsadminsMBP2.fios-router.home>
2020-06-27 07:51:11 -0400

    updated FpWriter

--- src/main/scala/hungerford/fp/FpWriter.scala
+++ src/main/scala/hungerford/fp/FpWriter.scala
@@ -1,11 +1,15 @@
 package hungerford.fp
 
-case class FpWriter[ T, A ]( value : T, context : Monoid[ A ] ) extends Monad[ FpWriter[ T, A ], T ] {
-    override def flatMap[ C, D ]( fn : T => Monad[ C, D ] ) : Monad[ C, D ] =  {
-        val newWriter = fn( value ).asInstanceOf[ FpWriter[ D, A ] ]
-        FpWriter[ D, A ]( newWriter.value, context.combine( newWriter.context ).asInstanceOf[ Monoid[ A ] ] ).asInstanceOf[ Monad[ C, D ] ]
+case class FpWriter[ T, A, B ]( value : T, context : Monoid[ A, B ] ) extends Monad[ ({ type U[X] = FpWriter[ X, A, B ] })#U, T ] {
+    override def flatMap[ R, S ]( a : FpWriter[ R, A, B ] )
+                                ( fn : R => FpWriter[ S, A, B ] ) : FpWriter[ S, A, B ] = a match {
+        case FpWriter( v : R, c : Monoid[ A, B ] ) =>
+            val FpWriter( newV, newC : Monoid[ A, B ] ) = fn( v )
+            FpWriter( newV, c.combineM( newC ) )
     }
 
-    override def unit[ C >: FpWriter[ T, A ], B >: T ]( ele : B ) : Monad[ C, B ] = FpWriter( ele, context.empty.asInstanceOf[ Monoid[ A ] ] ).asInstanceOf[ Monad[ C, B ] ]
+    override def unit[ C ]( ele : C ) : FpWriter[ C, A, B ] = FpWriter( ele, context.emptyM )
+
+    override def get[ C ]( ele : FpWriter[ C, A, B ] ) : Option[ C ] = Some( ele.value )
 }
 
--- src/main/scala/hungerford/fp/Monoid.scala
+++ src/main/scala/hungerford/fp/Monoid.scala
@@ -2,7 +2,9 @@ package hungerford.fp
 
 trait MonoidStatic [ T[ _ ] ] {
     def empty : T[ Nothing ]
+    def emptyM : Monoid[ T, Nothing ] = empty.asInstanceOf[ Monoid[ T, Nothing ] ]
     def combine[ B  ]( a : T[ B ], b : T[ B ] ) : T[ B ]
+    def combine[ B ]( a : Monoid[ T, B ], b : Monoid[ T, B ] ) : Monoid[ T, B ] = combine( a.native, b.native ).asInstanceOf[ Monoid[ T, B ] ]
 
     def concat[ B ]( eles : FpList[ T[ B ] ] ) : T[ B ] = eles match {
         case FpNil => empty.asInstanceOf[ T[ B ] ]
@@ -15,6 +17,10 @@ trait Monoid[ T[ _ ], +A ] extends MonoidStatic[ T ] {
 
     this : T[ A ] =>
         def combine[ B >: A ]( a : T[ B ] ) : T[ B ] = combine( this.asInstanceOf[ T[ B ] ], a )
+        def combine[ B >: A ]( a : Monoid[ T, B ] ) : T[ B ] = combine( a.native )
+        def combineM[ B >: A ]( a : Monoid[ T, B ] ) : Monoid[ T,  B ] = combine( this.monoid, a )
+        def native[ B >: A ] : T[ B ] = this.asInstanceOf[ T[ B ] ]
+        def monoid[ B >: A ] : Monoid[ T, B ] = this.asInstanceOf[ Monoid[ T, B ] ]
 
 }
 
